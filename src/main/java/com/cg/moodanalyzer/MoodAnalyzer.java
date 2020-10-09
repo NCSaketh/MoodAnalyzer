8b945f2101c06346efe5b425e53ddc6996509964
NCSaketh <nc.saketh1@gmail.com>
2020-10-09 23:30:51 +0530

    UC1 : adding analyze method

--- src/main/java/com/cg/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/cg/moodanalyzer/MoodAnalyzer.java
@@ -1,7 +1,20 @@
 package com.cg.moodanalyzer;
 
+import java.util.Scanner;
+
 public class MoodAnalyzer {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Mood Analyzer");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        System.out.println("Enter the mood");
+        String message = sc.nextLine();
+        analyzeMood(message);
     }
+
+    private static String analyzeMood(String message) {
+        return("Happy or Sad");
+    }
+
+
 }
