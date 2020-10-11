e4d2f6f0e012274c26a54e862575d80eb91b3e18
NCSaketh <nc.saketh1@gmail.com>
2020-10-11 17:09:05 +0530

    Removing parameters of analyzeMood

--- src/test/java/com/cg/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/cg/moodanalyzer/MoodAnalyzerTest.java
@@ -7,15 +7,15 @@ public class MoodAnalyzerTest {
 
     @Test
     public void givenMessage_WhenSadMood_ShouldReturnSAD() {
-        MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-        String result = MoodAnalyzer.analyzeMood("I am in Sad Mood");
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in Sad Mood");
+        String result = MoodAnalyzer.analyzeMood();
         Assert.assertEquals("SAD", result);
     }
 
     @Test
     public void givenMessage_InAnyMood_ShouldReturnHAPPY() {
-        MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-        String result = MoodAnalyzer.analyzeMood("I am in Any Mood");
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in Any Mood");
+        String result = MoodAnalyzer.analyzeMood();
         Assert.assertEquals("HAPPY", result);
     }
 }
